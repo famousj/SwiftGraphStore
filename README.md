299659d9d672563b6bdcd30bbc98ba76c772ea46
Justin LeBlanc <leblanc@wustl.edu>
2021-10-02 15:11:00 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SwiftGraphStore
+
+A description of this package.
---
+++ Sources/SwiftGraphStore/SwiftGraphStore.swift
@@ -0,0 +1,6 @@
+public struct SwiftGraphStore {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
