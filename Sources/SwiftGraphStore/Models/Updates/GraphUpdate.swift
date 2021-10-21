2022821ec00f3ad44301c4836319ccb91b29fac1
Justin LeBlanc <leblanc@wustl.edu>
2021-10-21 15:44:17 -0500

    Add a Keys GraphUpdate

--- Sources/SwiftGraphStore/Models/Updates/GraphUpdate.swift
+++ Sources/SwiftGraphStore/Models/Updates/GraphUpdate.swift
@@ -1,18 +1,21 @@
 import Foundation
+import UrsusHTTP
 
 public enum GraphUpdate: Codable, Equatable {
-
+    
     case addGraph(resource: Resource,
                   graph: [String: Graph],
-                  mark: String?,
+                  mark: Mark?,
                   overwrite: Bool)
     case addNodes(resource: Resource,
                   nodes: [String: Graph])
-
+    case keys(Keys)
     
     enum CodingKeys: String, CodingKey {
         case addGraph = "add-graph"
         case addNodes = "add-nodes"
+        
+        case keys
     }
     
     enum AddGraphCodingKeys: String, CodingKey {
@@ -42,6 +45,33 @@ public enum GraphUpdate: Codable, Equatable {
             var addNodesContainer = container.nestedContainer(keyedBy: AddNodesCodingKeys.self, forKey: .addNodes)
             try addNodesContainer.encode(resource, forKey: .resource)
             try addNodesContainer.encode(nodes, forKey: .nodes)
+        case let .keys(keys):
+            try container.encode(keys, forKey: .keys)
         }
+    }
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        if let keys = try? container.decode(Keys.self, forKey: .keys) {
+            self = .keys(keys)
+            return
+        }
+        
+        if let addGraphContainer = try? container.nestedContainer(keyedBy: AddGraphCodingKeys.self, forKey: .addGraph) {
+            let resource = try addGraphContainer.decode(Resource.self, forKey: .resource)
+            let graph = try addGraphContainer.decode([String: Graph].self, forKey: .graph)
+            let mark = try addGraphContainer.decodeIfPresent(Mark.self, forKey: .mark)
+            let overwrite = try addGraphContainer.decode(Bool.self, forKey: .overwrite)
+            self = .addGraph(resource: resource,
+                             graph: graph,
+                             mark: mark,
+                             overwrite: overwrite)
+            return
+        }
+        
+        let addNodesContainer = try container.nestedContainer(keyedBy: AddNodesCodingKeys.self, forKey: .addNodes)
+        let resource = try addNodesContainer.decode(Resource.self, forKey: .resource)
+        let nodes = try addNodesContainer.decode([String: Graph].self, forKey: .nodes)
+        self = .addNodes(resource: resource, nodes: nodes)
     }
 }
--- Tests/SwiftGraphStoreTests/UpdateDataTests.swift
+++ Tests/SwiftGraphStoreTests/UpdateDataTests.swift
@@ -5,6 +5,7 @@ import UrsusHTTP
 
 // TODO: Fill in all the nil values with real values and make sure things still work
 final class UpdateDataTests: XCTestCase {
+    
     func test_addGraph_pokeResponseParses() {
         let _: GraphStoreUpdate = try! JSONLoader.load("add-graph-poke.json")
     }
@@ -12,17 +13,23 @@ final class UpdateDataTests: XCTestCase {
     func test_addGraph_scryResponseParses() {
         let _: GraphStoreUpdate = try! JSONLoader.load("add-graph-scry.json")
     }
+    
+    func test_postParses() {
+        let _: Post = try! JSONLoader.load("post.json")
+    }
+
+    func test_addNodesParses() {
+        let _: GraphStoreUpdate = try! JSONLoader.load("add-nodes.json")
+    }
+    
+    func test_keysPares() {
+        let _: GraphStoreUpdate = try! JSONLoader.load("keys.json")
+    }
 
     func test_addGraphEncodeAndDecode() {
         let resource = Resource(ship: Ship.random, name: UUID().uuidString)
-        let post = Post(author: resource.ship,
-                        index: UUID().uuidString,
-                        timeSent: Date(),
-                        contents: nil,
-                        hash: nil,
-                        signatures: [])
-        let graph = Graph(post: post, children: nil)
-        let index = post.index
+        let graph = Graph.testInstance
+        let index = graph.post.index
         let testObject = GraphUpdate.addGraph(resource: resource,
                                               graph: [index:graph],
                                               mark: nil,
@@ -38,15 +45,7 @@ final class UpdateDataTests: XCTestCase {
         let decoded = try! decoder.decode(GraphUpdate.self, from: data)
         XCTAssertEqual(decoded, testObject)
     }
-    
-    func test_postParses() {
-        let _: Post = try! JSONLoader.load("post.json")
-    }
-
-    func test_addNodesParses() {
-        let _: GraphStoreUpdate = try! JSONLoader.load("add-nodes.json")
-    }
-    
+        
     func test_addNodesEncodeAndDecode() {
         let resource = Resource(ship: Ship.random, name: UUID().uuidString)
         let post = Post(author: resource.ship,
@@ -59,6 +58,22 @@ final class UpdateDataTests: XCTestCase {
         let index = post.index
         let testObject = GraphUpdate.addNodes(resource: resource,
                                               nodes: [index: graph])
+        
+        let encoder = JSONEncoder()
+        let data = try! encoder.encode(testObject)
+        
+        let jsonString = String(data: data, encoding: .utf8)!
+        print("JSON: \(jsonString)")
+        
+        let decoder = JSONDecoder()
+        
+        let decoded = try! decoder.decode(GraphUpdate.self, from: data)
+        XCTAssertEqual(decoded, testObject)
+    }
+    
+    func test_addKeysEncodeAndDecode() {
+        let keys = [ Resource.testInstance, Resource.testInstance ]
+        let testObject = GraphUpdate.keys(keys)
         
         let encoder = JSONEncoder()
         let data = try! encoder.encode(testObject)
