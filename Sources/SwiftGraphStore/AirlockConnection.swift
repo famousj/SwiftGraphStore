9dc32439039783be822f7a2edd439c31dd38ae87
Justin LeBlanc <leblanc@wustl.edu>
2021-10-21 14:04:24 -0500

    Add function definition to protocol

--- Sources/SwiftGraphStore/AirlockConnection.swift
+++ Sources/SwiftGraphStore/AirlockConnection.swift
@@ -10,4 +10,6 @@ public protocol AirlockConnection {
     func requestPoke<T: Encodable>(ship: Ship, app: App, mark: Mark, json: T) -> AnyPublisher<Never, PokeError>
     func requestScry<T: Decodable>(app: App, path: Path) -> AnyPublisher<T, AFError>
     func requestStartSubscription(ship: Ship, app: App, path: Path) -> AnyPublisher<Never, AFError>
+    
+    func requestTestScry(app: App, path: Path) -> AnyPublisher<String, AFError>
 }
