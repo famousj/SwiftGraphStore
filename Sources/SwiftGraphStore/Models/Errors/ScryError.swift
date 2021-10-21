0501902de29fcbca31b904a7ec89e210719cb950
Justin LeBlanc <leblanc@wustl.edu>
2021-10-20 21:20:08 -0500

    Add a scry error for when you aren't logged in.

--- Sources/SwiftGraphStore/Models/Errors/ScryError.swift
+++ Sources/SwiftGraphStore/Models/Errors/ScryError.swift
@@ -3,10 +3,13 @@ import Alamofire
 
 public enum ScryError: LocalizedError {
     
+    case notLoggedIn
     case scryFailed(message: String)
     
     public var errorDescription: String? {
         switch self {
+        case .notLoggedIn:
+            return "Unable read from ship: you aren't logged in!"
         case .scryFailed(let message):
             return "Unable to read from ship: \(message)"
         }
