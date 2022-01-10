62fba6e855ce726d395f747259a463f744bd8a48
Justin LeBlanc <leblanc@wustl.edu>
2022-01-10 15:39:09 -0600

    Refactor Fake GS connection to work like the fake airlock connection does viz publishers.

--- Sources/SwiftGraphStoreFakes/FakeGraphStoreConnection.swift
+++ Sources/SwiftGraphStoreFakes/FakeGraphStoreConnection.swift
@@ -5,7 +5,6 @@ import UrsusHTTP
 
 public class FakeGraphStoreConnection: GraphStoreConnecting {
     
-
     public init() {}
     
     public var ship: Ship?
@@ -37,86 +36,134 @@ public class FakeGraphStoreConnection: GraphStoreConnecting {
     }
     
     public var requestLogin_calledCount = 0
-    public var loginSubject: PassthroughSubject<Ship, LoginError> = PassthroughSubject()
+    public var requestLogin_response: Ship?
+    public var requestLogin_error: LoginError?
     public func requestLogin() -> AnyPublisher<Ship, LoginError> {
         requestLogin_calledCount += 1
         
-        return loginSubject
-            .eraseToAnyPublisher()
+        if let error = requestLogin_error {
+            return Fail(outputType: Ship.self, failure: error)
+                .eraseToAnyPublisher()
+        } else {
+            return Just(requestLogin_response ?? Ship.random)
+                .withErrorType()
+        }
     }
 
     public var requestConnect_calledCount = 0
-    public var connectSubject: PassthroughSubject<Never, PokeError> = PassthroughSubject()
+    public var requestConnect_error: PokeError?
     public func requestConnect() -> AnyPublisher<Never, PokeError> {
         requestConnect_calledCount += 1
         
-        return connectSubject
-            .eraseToAnyPublisher()
+        if let error = requestConnect_error {
+            return Fail(error: error)
+                .eraseToAnyPublisher()
+        }
+        else {
+            return neverPublisher()
+        }
     }
     
     public var requestStartSubcription_calledCount = 0
-    public var startSubscriptionSubject: PassthroughSubject<Never, StartSubscriptionError> = PassthroughSubject()
+    public var requestStartSubscription_error: StartSubscriptionError?
     public func requestStartSubscription() -> AnyPublisher<Never, StartSubscriptionError> {
         requestStartSubcription_calledCount += 1
-        return startSubscriptionSubject
-            .eraseToAnyPublisher()
+        
+        if let error = requestStartSubscription_error {
+            return Fail(error: error)
+                .eraseToAnyPublisher()
+        }
+        else {
+            return neverPublisher()
+        }
     }
     
     public var requestAddGraph_calledCount = 0
     public var requestAddGraph_paramResource: Resource?
-    public var addGraphSubject: PassthroughSubject<Never, PokeError> = PassthroughSubject()
+    public var requestAddGraph_error: PokeError?
     public func requestAddGraph(resource: Resource) -> AnyPublisher<Never, PokeError> {
         requestAddGraph_calledCount += 1
         requestAddGraph_paramResource = resource
         
-        return addGraphSubject
-            .eraseToAnyPublisher()
+        if let error = requestAddGraph_error {
+            return Fail(error: error)
+                .eraseToAnyPublisher()
+        }
+        else {
+            return neverPublisher()
+        }
     }
     
     public var requestAddNodes_calledCount = 0
     public var requestAddNodes_paramResource: Resource?
     public var requestAddNodes_paramIndex: String?
     public var requestAddNodes_paramPost: Post?
-    public var addNodesSubject: PassthroughSubject<Never, PokeError> = PassthroughSubject()
+    public var requestAddNodes_error: PokeError?
     public func requestAddNodes(resource: Resource, index: String, post: Post) -> AnyPublisher<Never, PokeError> {
         requestAddNodes_calledCount += 1
         requestAddNodes_paramResource = resource
         requestAddNodes_paramIndex = index
         requestAddNodes_paramPost = post
         
-        return addNodesSubject
-            .eraseToAnyPublisher()
+        if let error = requestAddNodes_error {
+            return Fail(error: error)
+                .eraseToAnyPublisher()
+        }
+        else {
+            return neverPublisher()
+        }
     }
     
     public var requestReadKeys_calledCount = 0
-    public var readKeysSubject: PassthroughSubject<GraphStoreUpdate, ScryError> = PassthroughSubject()
+    public var requestReadKeys_error: ScryError?
+    public var requestReadKeys_returnUpdate: GraphStoreUpdate?
     public func requestReadKeys() -> AnyPublisher<GraphStoreUpdate, ScryError> {
         requestReadKeys_calledCount += 1
         
-        return readKeysSubject
-            .eraseToAnyPublisher()
+        if let error = requestReadKeys_error {
+            return Fail(outputType: GraphStoreUpdate.self, failure: error)
+                .eraseToAnyPublisher()
+        }
+        else {
+            return Just(requestReadKeys_returnUpdate!)
+                .withErrorType()
+        }
     }
     
     public var requestReadGraph_calledCount = 0
     public var requestReadGraph_paramResource: Resource?
-    public var readGraphSubject: PassthroughSubject<GraphStoreUpdate, ScryError> = PassthroughSubject()
+    public var requestReadGraph_error: ScryError?
+    public var requestReadGraph_returnUpdate: GraphStoreUpdate?
     public func requestReadGraph(resource: Resource) -> AnyPublisher<GraphStoreUpdate, ScryError> {
         requestReadGraph_calledCount += 1
         requestReadGraph_paramResource = resource
         
-        return readGraphSubject
-            .eraseToAnyPublisher()
+        if let error = requestReadGraph_error {
+            return Fail(outputType: GraphStoreUpdate.self, failure: error)
+                .eraseToAnyPublisher()
+        }
+        else {
+            return Just(requestReadGraph_returnUpdate!)
+                .withErrorType()
+        }
     }
     
     public var requestReadRootNodes_calledCount = 0
     public var requestReadRootNodes_paramResource: Resource?
-    public var readRootNotesSubject: PassthroughSubject<GraphStoreUpdate, ScryError> = PassthroughSubject()
+    public var requestReadRootNodes_error: ScryError?
+    public var requestReadRootNodes_returnUpdate: GraphStoreUpdate?
     public func requestReadRootNodes(resource: Resource) -> AnyPublisher<GraphStoreUpdate, ScryError> {
         requestReadRootNodes_calledCount += 1
         requestReadRootNodes_paramResource = resource
         
-        return readRootNotesSubject
-            .eraseToAnyPublisher()
+        if let error = requestReadRootNodes_error {
+            return Fail(outputType: GraphStoreUpdate.self, failure: error)
+                .eraseToAnyPublisher()
+        }
+        else {
+            return Just(requestReadRootNodes_returnUpdate!)
+                .withErrorType()
+        }
     }
     
     public func requestTestScry(resource: Resource, path: Path) -> AnyPublisher<String, ScryError> {
