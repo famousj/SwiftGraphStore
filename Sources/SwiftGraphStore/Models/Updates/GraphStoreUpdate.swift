77a72e10ab7b8e791d68daf3dcbb103253c8b99c
Justin LeBlanc <leblanc@wustl.edu>
2022-01-12 15:32:36 -0600

    Make public initializer for GraphStoreUpdate, which is needed to write tests.

--- Sources/SwiftGraphStore/Models/Updates/GraphStoreUpdate.swift
+++ Sources/SwiftGraphStore/Models/Updates/GraphStoreUpdate.swift
@@ -2,6 +2,10 @@ import Foundation
 
 public struct GraphStoreUpdate: Codable, Equatable {
     public let graphUpdate: GraphUpdate
+
+    public init(graphUpdate: GraphUpdate) {
+        self.graphUpdate = graphUpdate
+    }
     
     enum CodingKeys: String, CodingKey {
         case graphUpdate = "graph-update"
