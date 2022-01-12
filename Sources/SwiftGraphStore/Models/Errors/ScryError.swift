5527ce05f68404c619db07d91bbb186bbab52bea
Justin LeBlanc <leblanc@wustl.edu>
2022-01-12 15:29:41 -0600

    Make a Scry error for when we get a 404, that we couldn't find a resource.

--- Sources/SwiftGraphStore/Models/Errors/ScryError.swift
+++ Sources/SwiftGraphStore/Models/Errors/ScryError.swift
@@ -4,12 +4,15 @@ import Alamofire
 public enum ScryError: LocalizedError {
     
     case notLoggedIn
+    case resourceNotFound(url: URL?)
     case scryFailed(message: String)
     
     public var errorDescription: String? {
         switch self {
         case .notLoggedIn:
             return "you aren't logged in!"
+        case .resourceNotFound(let url):
+            return "I couldn't find anything at " + (url ? url?.absoluteString : "your URL")
         case .scryFailed(let message):
             return message
         }
@@ -17,6 +20,12 @@ public enum ScryError: LocalizedError {
     
     // TODO: Add more expected errors
     static func fromAFError(_ error: AFError) -> ScryError {
-        .scryFailed(message: error.localizedDescription)
+        if case .responseValidationFailed(let reason) = error,
+           case .unacceptableStatusCode(let code) = reason,
+           code == 404 {
+            return .resourceNotFound(url: error.url)
+        }
+        
+        return .scryFailed(message: error.localizedDescription)
     }
 }
