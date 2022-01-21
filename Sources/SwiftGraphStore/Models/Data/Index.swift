a6fdd1c2ab2ca95a9cfb7832872242998ff31e95
Justin LeBlanc <leblanc@wustl.edu>
2022-01-21 09:27:20 -0600

    Allow creating an index from string and converting to a string two ways: one as a raw number and one formatted as a @ud

--- Sources/SwiftGraphStore/Models/Data/Index.swift
+++ Sources/SwiftGraphStore/Models/Data/Index.swift
@@ -7,8 +7,39 @@ struct Index {
     init(value: UInt64) {
         self.value = value
     }
+}
 
+extension Index {
     init(date: Date = .now) {
         self.init(value: UInt64(date.timeIntervalSinceReferenceDate * 1000))
+    }
+}
+
+extension Index {
+    init?(_ string: String) {
+        guard let stringValue = Self.valueFromString(string: string) else {
+            return nil
+        }
+        value = stringValue
+    }
+    
+    var stringWithSeparators: String {
+        Self.formatterWithDotSeparators.string(from: NSNumber(value: value)) ?? ""
+    }
+    
+    var string: String {
+        String(value)
+    }
+
+    private static func valueFromString(string: String) -> UInt64? {
+        let number = formatterWithDotSeparators.number(from: string)
+        return number?.uint64Value
+    }
+
+    private static var formatterWithDotSeparators: NumberFormatter {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.locale = Locale(identifier: "it_IT")
+        numberFormatter.numberStyle = .decimal
+        return numberFormatter
     }
 }
--- Tests/SwiftGraphStoreTests/IndexTests.swift
+++ Tests/SwiftGraphStoreTests/IndexTests.swift
@@ -8,4 +8,42 @@ class IndexTests: XCTestCase {
         let testObject = Index(date: Date.now)
         XCTAssertEqual(testObject.value, UInt64(milliseconds))
     }
+    
+    func test_string_formatsCorrectly() {
+        let testObject = Index("123456789")!
+        
+        let expectedString = "123.456.789"
+        XCTAssertEqual(testObject.stringWithSeparators, expectedString)
+    }
+    
+    func test_stringWithoutSeparators_formatsCorrectly() {
+        let testObject = Index("123456789")!
+        
+        let expectedString = "123456789"
+        XCTAssertEqual(testObject.string, expectedString)
+    }
+    
+    func test_init_acceptsFormattedString() throws {
+        let indexString = "123.456.789"
+        let testObject = try XCTUnwrap(Index(indexString))
+
+        XCTAssertEqual(testObject.stringWithSeparators, indexString)
+    }
+    
+    func test_init_truncates() throws {
+        let indexString = "1.234,56"
+        let testObject = try XCTUnwrap(Index(indexString))
+
+        let expectedString = "1.234"
+        XCTAssertEqual(testObject.stringWithSeparators, expectedString)
+    }
+    
+    func test_init_usesGroupsOfThree() throws {
+        let indexString = "1.234"
+        let testObject = try XCTUnwrap(Index(indexString))
+
+        let expectedString = "1.234"
+        XCTAssertEqual(testObject.stringWithSeparators, expectedString)
+    }
+
 }
