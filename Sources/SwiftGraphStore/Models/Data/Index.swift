ceadd4e95f61047df80fce38640c0f29df94068f
Justin LeBlanc <leblanc@wustl.edu>
2022-01-21 10:17:41 -0600

    Make properties/functions public

--- Sources/SwiftGraphStore/Models/Data/Index.swift
+++ Sources/SwiftGraphStore/Models/Data/Index.swift
@@ -1,41 +1,41 @@
 import Foundation
 
 // TODO: allow this index to be based off a @da, instead of a swift `Date`
-struct Index {
+public struct Index {
     let value: UInt64
-        
-    init(value: UInt64) {
+    
+    public init(value: UInt64) {
         self.value = value
     }
 }
 
 extension Index {
-    init(date: Date = .now) {
+    public init(date: Date = .now) {
         self.init(value: UInt64(date.timeIntervalSinceReferenceDate * 1000))
     }
 }
 
 extension Index {
-    init?(_ string: String) {
+    public init?(_ string: String) {
         guard let stringValue = Self.valueFromString(string: string) else {
             return nil
         }
         value = stringValue
     }
     
-    var stringWithSeparators: String {
+    public var stringWithSeparators: String {
         Self.formatterWithDotSeparators.string(from: NSNumber(value: value)) ?? ""
     }
     
-    var string: String {
+    public var string: String {
         String(value)
     }
-
+    
     private static func valueFromString(string: String) -> UInt64? {
         let number = formatterWithDotSeparators.number(from: string)
         return number?.uint64Value
     }
-
+    
     private static var formatterWithDotSeparators: NumberFormatter {
         let numberFormatter = NumberFormatter()
         numberFormatter.locale = Locale(identifier: "it_IT")
@@ -46,13 +46,13 @@ extension Index {
 
 extension Index: Codable {
     
-    init(from decoder: Decoder) throws {
+    public init(from decoder: Decoder) throws {
         let container = try decoder.singleValueContainer()
         let stringValue = try container.decode(String.self)
         value = Self.valueFromString(string: stringValue) ?? 0
     }
     
-    func encode(to encoder: Encoder) throws {
+    public func encode(to encoder: Encoder) throws {
         var container = encoder.singleValueContainer()
         try container.encode(self.string)
     }
@@ -63,13 +63,13 @@ extension Index: Equatable {}
 extension Index: Hashable {}
 
 extension Index: Comparable {
-    static func < (lhs: Index, rhs: Index) -> Bool {
+    public static func < (lhs: Index, rhs: Index) -> Bool {
         lhs.value < rhs.value
     }
 }
 
 extension Index: CustomStringConvertible {
-    var description: String {
+    public var description: String {
         String(value)
     }
 }
