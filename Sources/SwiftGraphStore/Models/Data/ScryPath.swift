aa52b88593221a281ae4539a5d8310fc707151f3
Justin LeBlanc <leblanc@wustl.edu>
2022-01-22 19:45:37 -0600

    Add an enum case for a node scry

--- Sources/SwiftGraphStore/Models/Data/ScryPath.swift
+++ Sources/SwiftGraphStore/Models/Data/ScryPath.swift
@@ -5,6 +5,8 @@ enum ScryPath {
     case keys
     case graph(resource: Resource)
     case rootNodes(resource: Resource)
+    // TODO: make an enum for Mode (%kith/%lone)
+    case node(resource: Resource, index: Index)
     
     var asPath: Path {
         switch self {
@@ -14,6 +16,8 @@ enum ScryPath {
             return graphPath(resource: resource)
         case .rootNodes(let resource):
             return graphPath(resource: resource) + "/subset/lone/~/~"
+        case let .node(resource, index):
+            return graphPath(resource: resource) + "/node/index/kith" + index.path
         }
     }
     
--- Tests/SwiftGraphStoreTests/ScryPathTests.swift
+++ Tests/SwiftGraphStoreTests/ScryPathTests.swift
@@ -1,6 +1,6 @@
 import XCTest
 import UrsusHTTP
-
+import BigInt
 @testable import SwiftGraphStore
 
 final class ScryPathTests: XCTestCase {
@@ -24,6 +24,24 @@ final class ScryPathTests: XCTestCase {
         let testObject = ScryPath.rootNodes(resource: resource)
         
         let expectedPath = "/graph/~nyt/graph-with-nodes/subset/lone/~/~"
+        XCTAssertEqual(testObject.asPath, expectedPath)
+    }
+    
+    func test_asPath_node_singleIndex() throws {
+        let resource = Resource(ship: Ship("nyt"), name: "graph-with-nodes")
+        let index = Index(value: BigUInt(1234))
+        let testObject = ScryPath.node(resource: resource, index: index)
+        
+        let expectedPath = "/graph/~nyt/graph-with-nodes/node/index/kith/1.234"
+        XCTAssertEqual(testObject.asPath, expectedPath)
+    }
+    
+    func test_asPath_node_multipleIndices() throws {
+        let resource = Resource(ship: Ship("nyt"), name: "graph-with-nodes")
+        let index = Index(values: [BigUInt(1), BigUInt(123456)])
+        let testObject = ScryPath.node(resource: resource, index: index)
+
+        let expectedPath = "/graph/~nyt/graph-with-nodes/node/index/kith/1/123.456"
         XCTAssertEqual(testObject.asPath, expectedPath)
     }
 }
