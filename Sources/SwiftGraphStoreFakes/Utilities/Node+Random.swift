7fef5a2378e72294532931a124bbf051d09bda93
Justin LeBlanc <leblanc@wustl.edu>
2022-02-21 10:50:08 -0600

    Make the node test instance public

--- Sources/SwiftGraphStoreFakes/Utilities/Node+Random.swift
+++ Sources/SwiftGraphStoreFakes/Utilities/Node+Random.swift
@@ -2,7 +2,7 @@ import Foundation
 @testable import SwiftGraphStore
 
 extension Node {
-    static var testInstance: Node {
+    public static var testInstance: Node {
         return Node(post: Post.testInstance, children: nil)
     }
 }
--- Tests/SwiftGraphStoreTests/TestInstances/Node+Random.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import SwiftGraphStore
-
-extension Node {
-    static var testInstance: Node {
-        Node(post: Post.testInstance, children: nil)
-    }
-}
